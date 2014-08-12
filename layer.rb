93c0eb0eade3bf4bcaa157e3418175848a0184f1
RPGP1 <great.super.science@gmail.com>
2014-08-12 09:55:16 +0900

    Create layer.rb

---
+++ layer.rb
@@ -0,0 +1,10 @@
+# coding: UTF-8
+
+Layer = {}
+Layer[:wave] = 0
+Layer[:character_body_up] = 6
+Layer[:character_body_down] = 5
+Layer[:character_back_up] = 4
+Layer[:character_back_down] = 3
+Layer[:character_base_up] = 2
+Layer[:character_base_down] = 1
